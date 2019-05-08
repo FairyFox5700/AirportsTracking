c84b9a221b6d8d9e214e3cfbbe5207680ffa5774
FairyFox5700 <46414904+FairyFox5700@users.noreply.github.com>
2019-05-09 00:18:33 +0300

    Add files via upload

---
+++ Airports/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Airports;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Graph.GetNextPorts();
+              Graph.DijkstraMinPath("DME", "LAS");
+            // Graph.PrintPorts("DME");
+            //Graph.SearchPointsWithBfs("DME", "ASF");
+            //AirlineData.WriteCsw();
+
+        }
+    }
+}
