cef1076befe12cceac1eb42fb87c24859d465454
Vadim Mulish <43036478+WalrusPUNCH@users.noreply.github.com>
2019-05-11 18:43:33 +0300

    Add main program file
    Програма запускається звідси

---
+++ MainProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airports
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph.AStarMinPath("DME", "FRA");
+            Console.WriteLine("\nDEIKSTA");
+            Graph.DijkstraMinPath("DME", "FRA");
+            Console.ReadKey();
+        }
+    }
+
+  
+}
